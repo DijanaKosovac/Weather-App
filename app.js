5ace36377a394a85d4c02f83a65d05514c130f43
DijanaKosovac <kosovacdijana@gmail.com>
2020-04-10 12:53:50 +0200

    'Weatherapp'

---
+++ app.js
@@ -0,0 +1,51 @@
+const api = {
+    key: 'c3b87e1f9a9d1e7216d579a3a765ceb2',
+    base: 'http://api.openweathermap.org/data/2.5/'
+}
+
+const searchbox = document.querySelector('.search-box');
+searchbox.addEventListener('keypress', setQuery);
+
+function setQuery(evt) {
+    if(evt.keyCode == 13) {
+        getResults(searchbox.value);
+    }
+}
+
+function getResults(query) {
+    fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`).then(weather => {
+        return weather.json();
+    }).then(displayResults);
+}
+
+function displayResults(weather) {
+    console.log(weather);
+    let city = document.querySelector('.location .city');
+    city.innerHTML = `${weather.name}, ${weather.sys.country}`;
+    
+    let now = new Date();
+    let date = document.querySelector('.location .date');
+    date.innerHTML = dateBuilder(now);
+
+    let temp = document.querySelector('.current .temp');
+    temp.innerHTML = `${Math.round(weather.main.temp).toFixed(0)}<span>&deg;C</span>`;
+
+    let weather_el = document.querySelector('.current .weather');
+    weather_el.innerHTML = weather.weather[0].main;
+
+    let hilow = document.querySelector('.hi-low');
+    hilow.innerText = `${Math.round(weather.main.temp_min)} °C / ${weather.main.temp_max} °C`;
+}
+
+
+function dateBuilder(d) {
+    let months = ['January', 'February', 'March', 'April', 'May','June', 'July', 'August', 'September', 'October','November','December'];
+    let days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
+}
